7197272f0490fd7f3d06af7bb62155f9b206166f
Raunak <54508518+TROUBLE-1@users.noreply.github.com>
2020-05-20 10:25:52 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'username');
-   define('DB_PASSWORD', 'password');
-   define('DB_DATABASE', 'shop_site');
-
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-
-?>
